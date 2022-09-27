e5b3ab03828b29b62ff4a368616ba0d2f29e6f72
parmstro@redhat.com <parmstro@parmstrong.ca>
2022-09-26 22:19:21 -0400

    creating selinuxlab project

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 ansible-galaxy collection install community.general
+ansible-galaxy collection install ansible.posix
 ansible-playbook -i inventory main.yml
+mkdir -p /home/student/src
+cd /home/student/src
+git clone https://github.com/parmstro/selinuxlab.git
+cp /home/student/selinux-workshop/inventory /home/student/src/selinuxlab/
+cd /home/student/src/selinuxlab/
+ansible-playbook -i inventory setup-testapp.yml
